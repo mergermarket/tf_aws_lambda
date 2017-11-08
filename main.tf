60c400da3e616b36aa477214fe7e2a6ba461b572
Tom Yandell <tom@tomyandell.co>
2017-11-08 17:10:39 +0000

    Remove cloudwatch events code
    This is just a lambda with no cron scheduling.

--- main.tf
+++ main.tf
@@ -17,25 +17,6 @@ resource "aws_lambda_function" "lambda_function" {
   }
 }
 
-resource "aws_cloudwatch_event_rule" "cron_schedule" {
-  name                = "${var.function_name}-cron_schedule"
-  description         = "This event will run according to a schedule for lambda ${var.function_name}"
-  schedule_expression = "${var.lambda_cron_schedule}"
-}
-
-resource "aws_cloudwatch_event_target" "event_target" {
-  rule      = "${aws_cloudwatch_event_rule.cron_schedule.name}"
-  arn       = "${aws_lambda_function.lambda_function.arn}"
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_function.function_name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.cron_schedule.arn}"
-}
-
 output "lambda_arn" {
   value = "${aws_lambda_function.lambda_function.arn}"
 }
