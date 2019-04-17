e0866622fe3fbf439832e5f5c1e5cfa64ef86b52
Keir Badger <keirbadger@hotmail.co.uk>
2019-04-17 21:25:03 +0100

    add cloudwatch logging setup

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ resource "aws_lambda_function" "lambda_function" {
     variables = "${var.lambda_env}"
   }
 }
+
+resource "aws_cloudwatch_log_group" "lambda_loggroup" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 7
+  depends_on      = ["aws_lambda_function.lambda_function"]
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "kinesis_log_stream" {
+  count           = "${var.datadog_log_subscription_arn != "" ? 1 : 0}"
+  name            = "kinesis-log-stream-${var.function_name}"
+  destination_arn = "${var.datadog_log_subscription_arn}"
+  log_group_name  = "${aws_cloudwatch_log_group.lambda_loggroup.name}"
+  filter_pattern  = ""
+  depends_on      = ["aws_lambda_function.lambda_function"]
+}
